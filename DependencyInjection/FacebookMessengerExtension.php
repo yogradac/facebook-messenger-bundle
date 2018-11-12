627558a6ad2f0a6c0688b1733226e93e01ec6321
yogradac@gmail.com <yoann.gauchard@gmail.com>
2018-11-12 10:59:47 +0100

    fix class name

--- DependencyInjection/FacebookMessengerExtension.php
+++ DependencyInjection/FacebookMessengerExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class FacebookbMessengerExtension extends Extension
+class FacebookMessengerExtension extends Extension
 {
     /**
      * {@inheritdoc}
