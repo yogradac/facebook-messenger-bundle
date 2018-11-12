a524d1b148aeba01ee860d189761ccfe0571d3a1
yogradac@gmail.com <yoann.gauchard@gmail.com>
2018-11-12 11:17:16 +0100

    fix namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,5 @@
 <?php
-namespace Yogradac\FbMessengerBundle\DependencyInjection;
+namespace Yogradac\FacebookMessengerBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
